71807c80dc3670147fc4ba104ec852f51abba227
Tester <TestTestTest1000@Test.com.ru>
2021-10-12 22:32:36 +0300

    add 'Header' component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-navbar>
+      <b-navbar-brand href="#">Blockchain monitor</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss"></style>
