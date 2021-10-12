e2d22a6f10bbabcc251b01bb3c3de3919b7b49ae
Tester <TestTestTest1000@Test.com.ru>
2021-10-12 22:53:49 +0300

    add 'Chart' component to 'Main' component

--- src/view/Main.vue
+++ src/view/Main.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <v-header></v-header>
+    <b-contaner>
+      <b-row>
+        <b-col>
+          <v-header></v-header>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <v-chart></v-chart>
+        </b-col>
+      </b-row>
+    </b-contaner>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header';
+import Chart from '@/components/Chart';
 
 export default {
   name: 'Main',
   components: {
-    VHeader: Header,
+    vHeader: Header,
+    vChart: Chart,
   },
 };
 </script>
