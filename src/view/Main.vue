bf1ced2710b08479df2da86ace3eef0b06817e2c
Tester <TestTestTest1000@Test.com.ru>
2021-10-12 22:33:29 +0300

    add 'Main' component

---
+++ src/view/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+
+export default {
+  name: 'Main',
+  components: {
+    VHeader: Header,
+  },
+};
+</script>
+
+<style lang="scss"></style>
