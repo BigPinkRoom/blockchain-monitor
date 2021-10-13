41125b2c50caf8f31812ed44da782b3c08c7703d
Tester <TestTestTest1000@Test.com.ru>
2021-10-13 12:56:34 +0300

    add Chart component

--- src/components/Chart/index.vue
+++ src/components/Chart/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div></div>
+  <b-container>
+    <b-row>
+      <b-col cols="12" v-for="chart in charts" :key="chart.id">
+        <chart-module :title="chart.title" :type="chart.type" />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {};
+// import ChartMarketPrice from './components/ChartMarketPrice';
+// import ChartDifficulty from './components/ChartDifficulty';
+// import ChartCostPerTransaction from './components/ChartCostPerTransaction';
+import ChartModule from './components/ChartModule';
+
+export default {
+  name: 'Chart',
+  components: {
+    ChartModule,
+  },
+  data() {
+    return {
+      charts: [
+        { id: 1, title: 'Market price', type: 'MarketPrice' },
+        { id: 2, title: 'Difficulty', type: 'Difficulty' },
+        { id: 3, title: 'Cost per transaction', type: 'CostPerTransaction' },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
