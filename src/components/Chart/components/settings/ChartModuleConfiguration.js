9ae23d3e5e30452b43eaaf6860e241ecd1bc88f3
Tester <TestTestTest1000@Test.com.ru>
2021-10-13 12:57:01 +0300

    add Chart component configuration file

---
+++ src/components/Chart/components/settings/ChartModuleConfiguration.js
@@ -0,0 +1,43 @@
+export function getOptions(settings) {
+  return {
+    chart: {
+      type: 'line',
+      height: 350,
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '55%',
+        endingShape: 'rounded',
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'straight',
+    },
+    title: {
+      text: settings.title,
+      align: 'center',
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'],
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: settings.categories,
+      labels: {
+        show: false,
+      },
+    },
+  };
+}
