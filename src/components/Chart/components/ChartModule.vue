db7c4f88400d8c7013d4122053c6c63d88987795
Tester <TestTestTest1000@Test.com.ru>
2021-10-13 12:55:40 +0300

    add Chart component

---
+++ src/components/Chart/components/ChartModule.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="chart">
+    <apexchart type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+import Apexchart from 'vue-apexcharts';
+import api from '@/api';
+import { getOptions } from './settings/ChartModuleConfiguration';
+
+export default {
+  name: 'ChartModule',
+  components: {
+    Apexchart,
+  },
+  props: {
+    title: String,
+    type: String,
+  },
+
+  data() {
+    return {
+      series: [
+        {
+          name: '',
+          data: [],
+        },
+      ],
+      categories: [],
+    };
+  },
+  computed: {
+    chartOptions() {
+      const settings = {
+        title: this.title,
+        categories: this.categories,
+      };
+
+      return getOptions(settings);
+    },
+  },
+  methods: {
+    async getChartData() {
+      const response = await api.chart[`get${this.type}`]();
+
+      this.series[0].data = response.seriesData;
+      this.categories = response.categories;
+    },
+  },
+  mounted() {
+    this.getChartData();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
